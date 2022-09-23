a8d73c096b2ea813f8e0c3f4ac4ce6def25ea08b
shafik720 <shafikrasel5@gmail.com>
2022-09-23 19:20:57 +0600

    sdfgsdg

---
+++ 41.js
@@ -0,0 +1,8 @@
+
+// split এর কাজ কি ? slice এর কাজ কি ? substr এর কাজ কি ? substring কি ? join এর কাজ কি ? 
+
+// কোন একটি এরে থেকে কোন নির্দিস্ট অংশ বা ইলেমেন্ট বাদ দিয়ে বাকী অংশগুলো দিয়ে একটি এরে তৈরি করার  জন্য split() ফাংশানটি ব্যবহার করা হয়। 
+
+let dummy = 'Where have all the flowers gone !';
+let x = dummy.split('e');
+console.log(x);
