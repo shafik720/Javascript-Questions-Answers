0015c35790db168131a225cc2879fb475fa4550d
shafik720 <shafikrasel5@gmail.com>
2022-09-23 11:13:46 +0600

    dfgfd

---
+++ 36.js
@@ -0,0 +1,19 @@
+
+//36. ধরো একটি এরেতে একই সংখ্যা অনেকবার আছে। ফাংশান দিয়ে এমন একটি প্রোগ্রাম লেখো যেখানে দেখাবে ওই সংখ্যাটার পজিশান নাম্বারগুলো কত কত। 
+
+let numbers = [25,10,54,69,87,32,87,14,2,69,10,56,10,47];
+
+const arr =(model)=>{
+    let x = [];
+    let dummy = model;
+    for(let element of dummy){
+        if(element===69){
+            x.push(dummy.indexOf(element));
+            dummy[dummy.indexOf(element)] = 0; 
+        }
+    }
+    return x;
+}
+
+console.log(arr(numbers));
+// console.log(numbers.indexOf(54));
