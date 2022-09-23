db3b1e27d563a6b03e0f3b05b2ac196cbb5f2d54
shafik720 <shafikrasel5@gmail.com>
2022-09-23 12:00:40 +0600

    wertew

---
+++ 39.js
@@ -0,0 +1,8 @@
+// কোন string কে lowercase এবং uppercase করার প্রোগ্রাম লেখো। 
+
+let first = 'Abir Rahman';
+let x = first.toLowerCase();
+console.log(x);
+
+x = first.toUpperCase();
+console.log(x);
