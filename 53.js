e0ee11efe9267876be38a6297bab98a2a080f82c
shafik720 <shafikrasel5@gmail.com>
2022-09-24 20:42:33 +0600

    dfgdg

--- 53.js
+++ 53.js
@@ -3,5 +3,13 @@
 class cars{
     brand;
     color;
-
+    start(){
+        console.log(`Start the damn ${this.brand} car`)
+    }
+    constructor(brandName, brandColor){
+        this.brand = brandName;
+        this.color = brandColor;        
+    }
 }
+let x = new cars('Mercedez', 'Yellow');
+x.start();
