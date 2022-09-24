543aa0617e0211dffa169adbc6a9510d34dcd1b9
shafik720 <shafikrasel5@gmail.com>
2022-09-24 20:57:04 +0600

    dfgdfg

--- 56.js
+++ 56.js
@@ -2,9 +2,12 @@
 let cars = {
     brand : 'audi',
     cc : 2500,
-    available : ['america', 'england', 'bangladesh', 'Japan'];
+    available : ['america', 'england', 'bangladesh', 'Japan'],
     origin : 'England'
 }
 
 let x = Object.keys(cars);
 let y = Object.values(cars);
+
+console.log(x);
+console.log(y);
