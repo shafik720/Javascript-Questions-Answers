5820ea4fcc6149ec29c599201444813588951882
shafik720 <shafikrasel5@gmail.com>
2022-09-24 21:08:52 +0600

    dfgdg

--- 59.js
+++ 59.js
@@ -1,2 +1,14 @@
 
 let content = document.getElementById('content');
+
+let counter = 10;
+
+let x = setInterval(()=>{
+    content.innerText = counter;
+    counter--;
+    if(counter<=-2){
+        content.innerText = 'Finished';
+        content.style.color = 'red';
+        clearInterval(x);
+    }
+},1000)
