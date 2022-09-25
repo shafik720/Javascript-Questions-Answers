b8cec403cc857f2079eb38fbc8cbe2fc9cbfad9f
shafik720 <shafikrasel5@gmail.com>
2022-09-25 11:54:46 +0600

    dfghdg

---
+++ 72.js
@@ -0,0 +1,11 @@
+
+let dummy = [
+    {brand: 'Iphone', price: 95000, country: "California", ram: 2},
+    {brand: 'Blackberry', price: 65000, country: "England", ram: 3},
+    {brand: 'Xiaomi', price: 25000, country: "China", ram: 6},
+    {brand: 'Huwaei', price: 45000, country: "China", ram: 5},    
+]
+
+let x = {brand: 'Walton', price: 35000, country: "Bangladesh", ram: 16};
+dummy.push(x);
+console.log(dummy);
