5e47dceb0989304196e3ee4ea47dcfe84f92bdd3
shafik720 <shafikrasel5@gmail.com>
2022-09-25 12:09:14 +0600

    fgdfg

---
+++ 74.js
@@ -0,0 +1,13 @@
+/* const car = 'toyota';
+
+if(car.toLowerCase()!=='bmw'){
+    console.log('I will not go');
+}else{
+    console.log('Yay lets go'); 
+}
+এই প্রোগ্রামটি এক লাইনে শর্টকাটে ternary অপারেটর দিয়ে করে দেখাও। 
+ */
+
+const car = 'toyota';
+
+car.toLowerCase()==='bmw' ? console.log('i will go') : console.log('i am not interested');
