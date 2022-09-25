4f3b30a6adac22b2069739683b13924ea7da4c56
shafik720 <shafikrasel5@gmail.com>
2022-09-25 11:05:05 +0600

    dfgdfg

--- 62.js
+++ 62.js
@@ -1,9 +1,12 @@
 
 
 document.getElementById('btn2').addEventListener('click', function(){
-
+    dummy2();
 })
 
-let dummy = () => {
-    
+let dummy2 = () => {
+    let x = confirm('You want to see the website location !');
+    if(x){
+        console.log(location.href);
+    }
 }
