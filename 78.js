d03b113fba11c83651d3328aa3fd5f2b341f79ba
shafik720 <shafikrasel5@gmail.com>
2022-09-25 12:52:09 +0600

    fdghdfgh

--- 78.js
+++ 78.js
@@ -1,1 +1,29 @@
+let product = document.getElementById('product');
+let price = document.getElementById('price');
 
+const operation = () => {
+        
+}
+
+let getCart = () => {
+    let locateCart = localStorage.getItem('cart');
+    let cartObj;
+    if(locateCart){
+        cartObj = JSON.parse(locateCart);
+    }else{
+        cartObj = {};
+    }
+    return cartObj;
+}
+
+let addLocalStorage = (model) => {
+    let dummy = getCart();
+    let x ; 
+    if(x[model] === dummy){
+        x[model] = x[model] + 1;
+    }else{
+        x[model] = 1;
+    }
+    let dummified = JSON.stringify(dummy);
+    localStorage.setItem('cart', dummified);
+}
