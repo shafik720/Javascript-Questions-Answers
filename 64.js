877c47c9f75752476039622121e572203fe82b70
shafik720 <shafikrasel5@gmail.com>
2022-09-25 11:31:25 +0600

    fghdth

---
+++ 64.js
@@ -0,0 +1,12 @@
+
+
+let car = {
+    brand : 'Audi',
+    color : 'Black',
+    cc : 2500,
+    country : ['America', 'Japan', 'France', 'England'],
+    year : 2021
+}
+
+let {country} = car;
+console.log(country[3]);
